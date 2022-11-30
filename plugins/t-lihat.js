2119aee9df5d127a713a5b693cb7858c8790a127
Not Dev <109721532+Nda22@users.noreply.github.com>
2022-11-30 21:47:12 +0700

    Update t-lihat.js

--- plugins/t-lihat.js
+++ plugins/t-lihat.js
@@ -3,13 +3,15 @@ let handler = async (m, { conn }) => {
 externalAdReply: {
 renderLargerThumbnail: true,
 mediaType: 1,
-title: wm,
+title: 'NdaaBotz-MD',
 thumbnail: await m.quoted.download()
 }
 }
 
 await conn.reply(m.chat, 'Preview Media Jika Kamu Malas Download', m, { contextInfo: adReply })
 }
+handler.tags = ['tools']
+handler.help = ['lihat']
 handler.command = /^(lihat)$/i
 
 module.exports = handler
