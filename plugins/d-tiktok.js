783cb6794ab9acbeca5cdf38f1e4a7f5abe84e85
Not Dev <109721532+Nda22@users.noreply.github.com>
2022-12-14 17:43:01 +0700

    Update d-tiktok.js

--- plugins/d-tiktok.js
+++ plugins/d-tiktok.js
@@ -1,10 +1,10 @@
-let axios = require('axios');
+let golek = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
-    if (res.status != 200) throw res.message;
-    if (!res) throw res.message;
-    conn.sendButtonVid(m.chat, res.video, `*Judul:* ${res.title}\n${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), 'Cara simpan digalery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan!', 'menu', usedPrefix + 'menu', m)
+let res = await golek(`https://saipulanuar.ga/api/download/tiktok?url=${args[0]}`)
+let uhuy = await res.json()
+let json = uhuy.result
+await conn.sendButtonVid(m.chat, json.video, sukses, wm, 'menu', '/menu', m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
